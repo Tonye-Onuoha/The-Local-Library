640d4ac796aa5fd0006c99790a4593d301219104
TONYE HUGO ONUOHA <tonyeonuoha@gmail.com>
2024-01-06 17:08:32 +0100

    password reset setup

--- local_library/settings.py
+++ local_library/settings.py
@@ -154,11 +154,23 @@ LOGOUT_REDIRECT_URL = 'logout'
 
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# Bootstrap
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Password Reset E-mail Setup
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'tonyeonuoha@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'nxsrtkhrtxqcgttq'
 
 # AWS CONFIGURATIONS
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
